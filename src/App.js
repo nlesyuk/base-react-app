0c4fc7d14396e803503c5aa5219813224f5fdcb3
Nazar Lesyuk <pronaza@gmail.com>
2022-10-21 22:38:11 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MyInput from './components/UI/input/MyInput';
 import MySelect from './components/UI/MySelect/MySelect';
 
 function App() {
+  const options = [
+    { value: 'title', name: 'Title' },
+    { value: 'body', name: 'Body' },
+  ]
+
   const [posts, setPosts] = useState([
     { id: 1, title: 'title', body: 'description' },
     { id: 2, title: 'asd', body: 'rrr' },
     { id: 3, title: 'dfdfs', body: 'hhh' }
   ])
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
 
   function createPost(newPost) {
     setPosts([...posts, { title: newPost.title, body: newPost.body }])
   }
-
   function removePost(post) {
     setPosts([...posts.filter(p => p.id !== post.id)])
   }
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const options = [
-    { value: 'title', name: 'Title' },
-    { value: 'body', name: 'Body' },
-  ]
   function sortPost(sort) {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
+
+  const sortedPosts = useMemo(() => {
+    console.log('sortfn')
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  }, [searchQuery, sortedPosts])
 
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <MyInput value={searchQuery} onChange={e => setSearchQuery(e.target.value)} placeholder="Search" />
       <MySelect value={selectedSort} options={options} onChange={sortPost} defaultValue='Sort by' />
-      {posts.length
-        ? <PostList remove={removePost} posts={posts} title={'list of item'} />
+      {sortedAndSearchedPosts.length
+        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'list of item'} />
         : <h1>Posts doesn't exist</h1>
       }
 
