86c9d361b3da4cd68dad51900bce6aa6c5f53bd8
Nazar Lesyuk <pronaza@gmail.com>
2022-11-20 18:21:10 +0200

    PostList animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import  React from 'react'
 import PostItem from './PostItem';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 export default function PostList({posts, title, remove}) {
   if (!posts.length) {
@@ -9,10 +10,18 @@ export default function PostList({posts, title, remove}) {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{ title }</h1>
-      {posts.map((post, index) => {
-          return <PostItem remove={remove} number={index+1} post={post} key={post.id} />
-        })
-      }
+      <TransitionGroup className="todo-list">
+        {posts.map((post, index) => {
+           return <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+              >
+              <PostItem remove={remove} number={index+1} post={post} key={post.id} />
+             </CSSTransition>
+          })
+        }
+      </TransitionGroup>
     </div>
   )
 }
