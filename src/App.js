4b6f6ac4f501b6899f712956f082ea574495e081
Nazar Lesyuk <pronaza@gmail.com>
2022-12-03 11:53:30 +0200

    pagination - in progress

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Loader from './components/UI/Loader/Loader';
 import { usePosts } from './hooks/usePosts'
 import { useFetching } from './hooks/useFetching'
 import PostService from './API/PostService'
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
   const options = [
@@ -19,9 +20,19 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [visible, setVisible] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [totalPage, setTotalPage] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(10)
+
+  // TODO: pagination remake with useMemo(don't rerender each time)
+  let pagesArray = getPagesArray(totalPage)
+
   const [fetchPosts, isPostsLoading, postsError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPage(getPageCount(totalCount, limit))
+    console.log('totalPage', totalPage)
   })
 
   function createPost(newPost) {
@@ -53,6 +64,9 @@ function App() {
         ? <Loader centered={true} />
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'list of item'} />
       }
+      {pagesArray.map(p =>
+        <MyButton>{p}</MyButton>
+      )}
 
     </div >
   );
---
+++ src/utils/pages.js
@@ -0,0 +1,14 @@
+export const getPageCount = (totalCount, limit) => {
+  return Math.ceil(totalCount / limit)
+}
+
+// TODO: make hook usePagination
+export const getPagesArray = (totalPages) => {
+  const arr = []
+  for (let i = 0; i < totalPages; i++) {
+    arr.push(i + 1)
+  }
+
+  console.log(arr)
+  return arr
+}
