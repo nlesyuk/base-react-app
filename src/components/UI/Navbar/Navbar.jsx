a56d5c8f0b1cd4177186b9d71a2db3c97c25a53c
Nazar Global <nazariy.lesyuk@gmail.com>
2022-12-24 10:20:18 +0200

    add redirect while login/logout

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,22 +1,31 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../../context/useContext'
 import MyButton from '../button/MyButton'
 
+import classes from './Navbar.module.css'
+
 export default function Navbar() {
+  const navigate = useNavigate()
   const {isAuth, setIsAuth} = useContext(AuthContext)
 
   const logout = e => {
     e.preventDefault()
     setIsAuth(false)
     localStorage.removeItem('auth')
+    navigate('/login')
   }
 
   return (
-      <nav>
-        <MyButton onClick={logout}>logout</MyButton>
+      <nav className={classes.nav}>
         <Link to="/about">about</Link>
         <Link to="/posts">posts</Link>
+        <div className={classes.button}>
+        {isAuth
+          ? <MyButton onClick={logout}>logout</MyButton>
+          : <Link to="/login">login</Link>
+        }
+        </div>
       </nav>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import MyButton from '../components/UI/button/MyButton'
 import MyInput from '../components/UI/input/MyInput'
 import { AuthContext } from '../context/useContext'
 
 export default function Login() {
+  const navigate = useNavigate()
   const {isAuth, setIsAuth} = useContext(AuthContext)
 
   const login = e => {
     e.preventDefault()
     setIsAuth(true)
     localStorage.setItem('auth', 'true')
+    navigate('/posts')
   }
 
   return (
