3fd74d650f9e0049acff77116742d57020fd98d9
Nazar Global <nazariy.lesyuk@gmail.com>
2022-12-24 21:17:41 +0200

    login add credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import MyButton from '../components/UI/button/MyButton'
 import MyInput from '../components/UI/input/MyInput'
@@ -7,8 +7,11 @@ import { AuthContext } from '../context/useContext'
 export default function Login() {
   const navigate = useNavigate()
   const {isAuth, setIsAuth} = useContext(AuthContext)
+  const [username, setUsername] = useState('user')
+  const [password, setPassword] = useState('`1234567890`')
 
   const login = e => {
+    console.table(username, password)
     e.preventDefault()
     setIsAuth(true)
     localStorage.setItem('auth', 'true')
@@ -18,8 +21,18 @@ export default function Login() {
   return (
     <form>
       <h1>Login page</h1>
-      <MyInput type="text" placeholder='login'/>
-      <MyInput type="password" placeholder='password'/>
+      <MyInput
+        type="text"
+        placeholder='login'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <MyInput
+        type="password"
+        placeholder='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <MyButton onClick={login}>submit</MyButton>
     </form>
   )
